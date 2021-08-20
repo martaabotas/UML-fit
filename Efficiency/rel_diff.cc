7afc1f609a17c0f08865d9618ccb962952776b19
Marta André Botas <u21mbotas@pauli01.ncg.ingrid.pt>
2021-08-20 21:26:28 +0100

    relative diff graph

--- Efficiency/rel_diff.cc
+++ Efficiency/rel_diff.cc
@@ -14,8 +14,15 @@
 #include <RooWorkspace.h>
 #include <RooDataSet.h>
 #include <TH1F.h>
+#include <cstdlib>
 
 void rel_diff() {
+
+  /*TString input_file_eff = Form("/home/t3cms/u21mbotas/efficiency/UML-fit/Efficiency/eff_%i.root",year,year);
+  TString input_file_wei = Form("/home/t3cms/u21mbotas/efficiency/UML-fit/Efficiency/wei_%i.root",year,year);
+
+  TFile* f_eff = new TFile(input_file_eff);
+  TFile* f_wei = new TFile(input_file_wei);*/
 
   TFile* f_eff = new TFile("/home/t3cms/u21mbotas/efficiency/UML-fit/Efficiency/eff_2016.root");
   TFile* f_wei = new TFile("/home/t3cms/u21mbotas/efficiency/UML-fit/Efficiency/wei_2016.root");
@@ -25,9 +32,12 @@ void rel_diff() {
 
   Double_t efficiency_2016[8];
   Double_t wei_efficiency_2016[8];
+  Double_t diff[8];
+  Double_t relative_diff[8];
+  Double_t bins[] = {0, 1, 2, 3, 4, 5, 6, 7};
   Int_t n = 8;
 
-  TCanvas *c1 = new TCanvas("c1","Efficiency",200,10,500,300);
+  TCanvas *c1 = new TCanvas("c1","Relative Difference",200,10,500,300);
   //TCanvas *c2 = new TCanvas("c2","Weighted Efficiency",200,10,500,300);
   
   for(int i=0; i<n; i++) {
@@ -36,14 +46,21 @@ void rel_diff() {
     cout << "eff - " << efficiency_2016[i] << endl;
     wei_efficiency_2016[i]= wei_2016->GetEfficiency(i+1);
     cout << "wei - " << wei_efficiency_2016[i] << endl;
+    diff[i] = abs(efficiency_2016[i]-wei_efficiency_2016[i]);
+    cout << "diff - " << diff[i] << endl;
+    relative_diff[i] = diff[i]/efficiency_2016[i];
+    cout << "rel dif - " << relative_diff[i]*100 << " %" << endl;
     cout << " " << endl;
-
+    
   }
   
-  //cout << "graph" << endl;
-  
-  auto *gr = new TGraph(n,efficiency_2016,wei_efficiency_2016);
-  gr->Draw();
-  gr->SaveAs("/home/t3cms/u21mbotas/efficiency/UML-fit/Efficiency/comparison.gif");
+  TGraph* gr1 = new TGraph(n,bins,relative_diff);
+  gr1->Draw("AP");
+  gr1->SetMarkerColor(kBlue);
+  gr1->SetMarkerStyle(kFullDotLarge);
+  gr1->SetTitle("Efficiency relative difference"); //eff and wei eff diff
+  gr1->GetXaxis()->SetTitle("q^{2} bins");
+  gr1->GetYaxis()->SetTitle("Relative difference");
+  c1->SaveAs("/home/t3cms/u21mbotas/efficiency/UML-fit/Efficiency/relative_diff_2016.gif");
 
 }
