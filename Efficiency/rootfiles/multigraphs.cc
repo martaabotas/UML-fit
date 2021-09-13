90a8fa7a16e42a739501a4543ea8e664cd28443d
Marta André Botas <u21mbotas@pauli04.ncg.ingrid.pt>
2021-09-13 07:53:35 +0100

    x axis

--- Efficiency/rootfiles/multigraphs.cc
+++ Efficiency/rootfiles/multigraphs.cc
@@ -33,12 +33,12 @@ void multigraphs(int year) {
   TFile* f_nom_eff = new TFile(Form("./nom_eff_%i.root",year));
   TFile* f_wei_bEta = new TFile(Form("./wei_eff_%i_bEta.root",year));
   TFile* f_wei_k = new TFile(Form("./wei_eff_%i_kstTrkpEta.root",year));
-  //TFile* f_wei_bPt = new TFile(Form("./wei_eff_%i_bPt.root",year));
+  TFile* f_wei_bPt = new TFile(Form("./wei_eff_%i_bPt.root",year));
 
   TGraph* nom_eff = (TGraph*)f_nom_eff->Get("gr2");
   TGraph* wei_bEta = (TGraph*)f_wei_bEta->Get("gr3");
   TGraph* wei_k = (TGraph*)f_wei_k->Get("gr3");
-  //TGraph* wei_bPt = (TGraph*)f_wei_bPt->Get("gr3");
+  TGraph* wei_bPt = (TGraph*)f_wei_bPt->Get("gr3");
 
   //plot multigraph
 
@@ -47,14 +47,14 @@ void multigraphs(int year) {
   mg1->Add(nom_eff);
   mg1->Add(wei_bEta);
   mg1->Add(wei_k);
-  //mg1->Add(wei_bPt);
+  mg1->Add(wei_bPt);
   
   auto leg1 = new TLegend(0.65,0.7,0.9,0.88);
   leg1->Draw();
   leg1->AddEntry(nom_eff,"Unweighted", "p");
   leg1->AddEntry(wei_bEta, "Weight: bEta", "p");
   leg1->AddEntry(wei_k, "Weight: kstTrkpEta", "p");
-  //leg1->AddEntry(wei_bPt, "Weight: bPt", "p");
+  leg1->AddEntry(wei_bPt, "Weight: bPt", "p");
   leg1->SetBorderSize(0);
 
   TCanvas c1;
@@ -63,6 +63,7 @@ void multigraphs(int year) {
   mg1->Draw("AP");
   leg1->Draw();
   mg1->GetXaxis()->SetTitle("q^{2} [GeV^{2}]");
+  mg1->GetYaxis()->SetTitle("Efficiency Systematic Uncertainty");
 
   c1.SaveAs(Form("/home/t3cms/u21mbotas/efficiency/UML-fit/Efficiency/rootfiles/effs_%i.gif",year));
 
@@ -70,11 +71,11 @@ void multigraphs(int year) {
 
   TFile* f_rel_bEta = new TFile(Form("./rel_diff_%i_bEta.root",year));
   TFile* f_rel_k = new TFile(Form("./rel_diff_%i_kstTrkpEta.root",year));
-  //TFile* f_rel_bPt = new TFile(Form("./rel_diff_%i_bPt.root",year));
+  TFile* f_rel_bPt = new TFile(Form("./rel_diff_%i_bPt.root",year));
 
   TGraph* rel_bEta = (TGraph*)f_rel_bEta->Get("gr1");
   TGraph* rel_k = (TGraph*)f_rel_k->Get("gr1");
-  //TGraph* rel_bPt = (TGraph*)f_rel_bPt->Get("gr1");
+  TGraph* rel_bPt = (TGraph*)f_rel_bPt->Get("gr1");
 
   //plot multigraph
 
@@ -82,13 +83,13 @@ void multigraphs(int year) {
 
   mg2->Add(rel_bEta);
   mg2->Add(rel_k);
-  //mg2->Add(rel_bPt);
+  mg2->Add(rel_bPt);
   
   auto leg2 = new TLegend(0.65,0.7,0.9,0.88);
   leg2->Draw();
   leg2->AddEntry(rel_bEta, "Weight: bEta", "p");
   leg2->AddEntry(rel_k, "Weight: kstTrkpEta", "p");
-  //leg2->AddEntry(rel_bPt, "Weight: bPt", "p");
+  leg2->AddEntry(rel_bPt, "Weight: bPt", "p");
   leg2->SetBorderSize(0);
 
   TCanvas c2;
